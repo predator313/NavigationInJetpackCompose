01b7dca2f43f2ae0d599943018657b20ec2497bd
Android Dev <aamirnasa97@gmail.com>
2024-01-24 02:07:07 +0530

    navigation is done

--- app/src/main/java/com/aamirashraf/navigationincompose/Navigation.kt
+++ app/src/main/java/com/aamirashraf/navigationincompose/Navigation.kt
@@ -1,14 +1,91 @@
 package com.aamirashraf.navigationincompose
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 @Composable
 fun Navigation(
+) {
+    val navHostController = rememberNavController()
+    NavHost(navController = navHostController, startDestination = Screen.MainScreen.route) {
+        composable(route = Screen.MainScreen.route) {
+            MainScreen(navController = navHostController)
+        }
+
+        //we also pass arguments in the detail screen navigation
+        composable(route=Screen.DetailedScreen.route+"/{name}",  //"/{name} means this is compulsory argument like urls
+            arguments = listOf(
+                navArgument("name"){
+                    type= NavType.StringType
+                }
+            )
+            ){entry->
+            DetailScreen(name =entry.arguments?.getString("name") )
+        }
+
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(50.dp)
+    ) {
+        TextField(
+            value = text, onValueChange = {
+                text = it
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(
+            onClick = {
+                      navController.navigate(Screen.DetailedScreen.withArgs(text))
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = "To Detail Screen")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(
+    name:String?
 ){
-    val navHostController= rememberNavController()
-    NavHost(navController = navHostController, startDestination = Screen.MainScreen.route){
-        
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(text = "Hello $name")
     }
 }
--- app/src/main/java/com/aamirashraf/navigationincompose/Screen.kt
+++ app/src/main/java/com/aamirashraf/navigationincompose/Screen.kt
@@ -2,6 +2,34 @@ package com.aamirashraf.navigationincompose
 
 sealed class Screen(val route:String){
     //only the class inside the sealed class can inherit the sealed class
-    object MainScreen:Screen("main_screen")
-    object DetailedScreen:Screen("detail_screen")
+    data object MainScreen:Screen("main_screen")
+    data object DetailedScreen:Screen("detail_screen")
+
+    //lets understand little bit about vararg also in kotlin
+    /*
+
+
+    In the Kotlin programming language,
+     vararg is a keyword used to define a variable number of arguments in a function parameter.
+     It allows a function to accept an arbitrary number of arguments of a specified type.
+    val result1 = withArgs("one", "two", "three")
+    val result2 = withArgs("apple", "orange", "banana", "grape")
+    fun withArgs(vararg arg: String): String {
+        // 'arg' is an array of Strings
+        return arg.joinToString(", ")  // Join the strings with a comma and space
+    }
+
+     */
+
+
+
+    fun withArgs(vararg args:String):String{
+        return buildString {
+            append(route)
+            args.forEach {arg->
+                append("/$arg")
+
+            }
+        }
+    }
 }
